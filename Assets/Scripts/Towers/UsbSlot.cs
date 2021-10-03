ee0a912e54e41fba5a04b8ac43fab3ebd1575911
Jordan Kniest <j.kniest@basecom.de>
2021-10-03 15:35:23 +0200

    Add glow border shader for fields

---
+++ Assets/Scripts/Towers/UsbSlot.cs
@@ -0,0 +1,15 @@
+namespace Towers
+{
+    public class UsbSlot : Tower
+    {
+        protected override void OnPlace()
+        {
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}

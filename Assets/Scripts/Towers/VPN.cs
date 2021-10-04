d0ba5bb6d6ff083f7974b517e16f32790c2d5ee6
Jordan Kniest <j.kniest@basecom.de>
2021-10-04 19:32:13 +0200

    hole?

--- Assets/Scripts/Towers/VPN.cs
+++ Assets/Scripts/Towers/VPN.cs
@@ -1,10 +1,15 @@
+using UnityEngine;
+
 namespace Towers
 {
     public class VPN : Tower
     {
+        public Mesh defaultFieldMesh;
+        public Mesh holeFieldMesh;
+        
         protected override void OnPlace()
         {
-            
+            field.GetComponent<MeshFilter>().mesh = holeFieldMesh;
         }
 
         protected override void OnUpdate()
